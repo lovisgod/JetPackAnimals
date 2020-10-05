e7e32edc44b8fa19fefd7c55bc5163948c181de5
lovisgod <olifedayo94@gmail.com>
2020-10-05 06:35:28 +0100

    setup retrofit

--- app/src/main/java/com/example/animals/model/Animal.kt
+++ app/src/main/java/com/example/animals/model/Animal.kt
@@ -1,7 +1,35 @@
 package com.example.animals.model
 
+import com.google.gson.annotations.SerializedName
+import java.net.ProtocolFamily
+
 
 // a data class does not need a body
 data class Animal (
-    val name : String?
+    val name : String?,
+    val taxonomy: Taxonomy?,
+    val speed: Speed?,
+    val diet: String?,
+
+    @SerializedName("lifespan")
+    val lifeSpan: String?,
+
+    @SerializedName("image")
+    val imageUrl: String?
+)
+
+data class ApiKey (
+    val message: String?,
+    val key: String?,
+)
+
+data class Taxonomy (
+    val kingdom: String?,
+    val order: String?,
+    val family: String?
+)
+
+data class Speed(
+    val metric: String?,
+    val imperial: String?
 )
---
+++ app/src/main/java/com/example/animals/model/AnimalApiService.kt
@@ -0,0 +1,31 @@
+package com.example.animals.model
+
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * the gsonconverter factory converts the Json that we get from backend
+ * to our model class
+ *
+ * The Rxjava call adapter factory converts the responses into singletons that
+ * are observable**/
+
+class AnimalApiService {
+    private val BASE_URL = "https://us-central1-apis-467e.cloudfunctions.net"
+    private val API = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+        .create(AnimalApi::class.java)
+
+    fun getApiKey(): Single<ApiKey> {
+        return API.getApiKey()
+    }
+
+    fun getAnimals(key: String): Single<List<Animal>> {
+        return  API.getAnimals(key)
+    }
+}
